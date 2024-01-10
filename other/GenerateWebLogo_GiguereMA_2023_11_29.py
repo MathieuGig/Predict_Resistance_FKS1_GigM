6a67343658ad51d8cf94f08d506c9b3556f286a0
Mathieu Giguère <102560406+MathieuGig@users.noreply.github.com>
2024-01-10 10:37:56 -0500

    Reorganise Github

--- other/GenerateWebLogo_GiguereMA_2023_11_29.py
+++ other/GenerateWebLogo_GiguereMA_2023_11_29.py
@@ -1,3 +1,8 @@
+## Title: GenerateWebLogo
+## Author: Mathieu Giguere
+## Brief: Uses regex to format a list of strings into a file with 1 string per line,
+##        then prints a command for "weblogo3" that can be copy-pasted into a terminal.
+
 import pandas as pd
 import numpy as np
 import re
